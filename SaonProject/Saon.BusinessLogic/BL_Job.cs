f7b15e3b9d17eb2fff71a9e0ce6b30b35c59af3f
Yohalmo Castillo <yohalmovcr2012@gmail.com>
2020-05-25 16:41:24 -0600

    Creating Business Logic methods to our project

---
+++ SaonProject/Saon.BusinessLogic/BL_Job.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Saon.DataAccess;
+using Saon.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saon.BusinessLogic
+{
+    public class BL_Job
+    {
+        /// <summary>
+        /// This is the select equivalent, getting all job's list
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<Job>> ListJobs()
+        {
+            using (var db = new TestContext())
+            {
+                return await db.Jobs.ToListAsync();
+            }
+        }
+
+        /// <summary>
+        /// Getting a specific Job by Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<Job> GetJob(int id)
+        {
+            using (var db = new TestContext())
+            {
+                return await db.Jobs.FirstOrDefaultAsync(m => m.IdJob == id);
+            }
+        }
+
+        /// <summary>
+        /// Creating a new Job Entity 
+        /// </summary>
+        /// <param name="newJob"></param>
+        /// <returns></returns>
+        public async Task<int> CreateJob(Job newJob)
+        {
+            using (var db = new TestContext())
+            {
+                db.Jobs.Add(newJob);
+                return await db.SaveChangesAsync();
+            }
+        }
+
+
+        /// <summary>
+        /// Updating a specific job
+        /// </summary>
+        /// <param name="updateJob"></param>
+        /// <returns></returns>
+         public async Task<int> UpdateJob(Job updatedJob)
+        {
+            using (var db = new TestContext())
+            {
+                db.Jobs.Update(updatedJob);
+                return await db.SaveChangesAsync();
+            }
+        }
+
+        /// <summary>
+        /// Deliting a specific Job
+        /// </summary>
+        /// <param name="deletedJob"></param>
+        /// <returns></returns>
+        public async Task<int> DeleteJob(Job deletedJob)
+        {
+            using (var db = new TestContext())
+            {
+                db.Jobs.Remove(deletedJob);
+                return await db.SaveChangesAsync();
+            }
+        }
+
+        /// <summary>
+        /// Determinates if an object in database exists
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+
+        public bool JobExists(int id)
+        {
+            using (var db = new TestContext())
+            {
+                return db.Jobs.Any(e => e.IdJob == id);
+            }
+        }
+
+    }
+}
