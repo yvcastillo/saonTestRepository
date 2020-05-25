8a6e25977a1663e4787d1724bbe18d4e9e2d3dbf
Yohalmo Castillo <yohalmovcr2012@gmail.com>
2020-05-25 15:39:45 -0600

    Creating Job entity

--- SaonProject/Saon.Entities/Job.cs
+++ SaonProject/Saon.Entities/Job.cs
@@ -1,8 +1,26 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Saon.Entities
 {
     public class Job
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdJob { get; set; }
+        
+        [StringLength(64) ]
+        [Required]
+        public string JobTitle { get; set; }
+
+        [StringLength(256)]
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        [Required]
+        public DateTime ExpiredAt { get; set; }
     }
 }
